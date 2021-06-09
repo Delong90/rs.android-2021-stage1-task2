48af9d20c72263df2fa149d11f42c19503bcb247
Delong90 <matushkin030790@gmail.com>
2021-06-09 20:16:30 +0300

    feat: completed task1-task3

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,27 @@
+
 package subtask1
+
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 
 class DateFormatter {
 
-    // TODO: Complete the following function
+
+
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        var result = "Такого дня не существует"
+        var formatter = DateTimeFormatter.ofPattern("dd MMMM, EEEE")
+        try { result = LocalDate.of(year.toInt(),month.toInt(),day.toInt()).format(formatter.withLocale(
+            Locale.forLanguageTag("Ru")))
+
+        }
+        finally {
+            return result
+        }
     }
 }
+
+
--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,49 @@
 package subtask2
 
+import java.util.*
+fun main(args:Array<String>) {
+    var test:Abbreviation = Abbreviation()
+    println(test.abbreviationFromA("ytrewq","RY"))
+
+}
+
+
+
 class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var result = "NO"
+        val splitAbbreviationA = a.toUpperCase().split("")
+        val splitAbbreviationB = b.split("")
+        var i = 1
+        var j = 1
+        var index = 0
+
+
+        while (i<=a.length){
+            if (splitAbbreviationB[j] == splitAbbreviationA[i]){
+                j++
+                index++
+            }
+            i++
+        }
+        if (index == b.length) {result = "YES"}
+        return result
+
     }
 }
+
+
+
+//while (j<=b.length){
+//    if (splitAbbreviationB[j] in splitAbbreviationA){
+//        if ((splitAbbreviationA.indexOf(splitAbbreviationB[j])-index)>0) {
+//            index = splitAbbreviationA.indexOf(splitAbbreviationB[j])
+//            i++
+//            j++
+//        }else {j++}
+//    }else {j++}
+//}
+//if (i-1 == b.length) {result = "YES"}
+//return result
